f406df4c77daf3941b5a3d08d833b564588d1d01
Pritish Rajpurohit <workforpritish@gmail.com>
2026-01-31 19:02:17 +0530

    + Used Fiebase for authentication and data storage. + Created admin panel to manage blog posts.

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAcwKxAe92q4QWY8nEVBmnUE_mlhYUBHiQ",
+    authDomain: "smashcoder-blog.firebaseapp.com",
+    projectId: "smashcoder-blog",
+    storageBucket: "smashcoder-blog.firebasestorage.app",
+    messagingSenderId: "732015314078",
+    appId: "1:732015314078:web:ecbe8118cffd138386a78f",
+    measurementId: "G-F66EBNVLNP"
+};
