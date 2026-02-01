be896e8235131386b5e627ba5b32cfec6dbd064f
Pritish Rajpurohit <91045061+smashcoder@users.noreply.github.com>
2026-02-01 20:31:19 +0530

    Update blogData via Admin Panel

--- blogData.js
+++ blogData.js
@@ -1,4 +1,18 @@
 // This file contains all the diary entries.
 // You can edit this file to add, remove, or change blog posts.
 
-const blogData = [];
+const blogData = [
+    {
+        "id": 1,
+        "date": "2026-02-01",
+        "title": "Developing using AI",
+        "summary": "In this blog, I have shared how can AI be used in developing.",
+        "content": "Hi Everyone,\n\nToday I am using Ai for developing a project. It's great and giving amazing response. The AI does make mistakes but it all depends on how will we give the prompt.",
+        "mood": "Focused",
+        "weather": "Cloudy",
+        "tags": [
+            "Developing"
+        ],
+        "image": "https://www.chieflearningofficer.com/2022/10/26/developing-leaders-for-now-the-learning-well-of-experience/"
+    }
+];
